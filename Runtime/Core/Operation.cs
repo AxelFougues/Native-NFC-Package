564054013702a7e4d5bbe2d4dd5fcacc2fccdb9a
unknown <azfougues@gmail.com>
2024-06-16 09:33:04 -0400

    Update Lib

--- Runtime/Core/Operation.cs
+++ Runtime/Core/Operation.cs
@@ -1,8 +1,27 @@
+using AbyssWalkerDev.NativeNFC;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
 [Serializable]
 public class Operation : MonoBehaviour{
-    
+
+    public enum StandardComplexOperation {
+        None,
+        ChainedCommand,
+        DeepScan,
+        PowerScan,
+        WriteNDEF,
+        ReadNDEF
+    }
+    public class ChainedCommand {
+        List<byte> command;
+        List<byte> expectedReply;
+    }
+
+    public StandardComplexOperation standardComplexOperation;
+    public NDEFContent ndefMessage;
+    public List<ChainedCommand> commands;
+
 }
