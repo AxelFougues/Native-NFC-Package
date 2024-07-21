7448f1ea23be299404d7c0f2cae8433b8f557856
unknown <azfougues@gmail.com>
2024-07-21 17:12:58 -0400

    Update Operation.cs

--- Runtime/Core/Operation.cs
+++ Runtime/Core/Operation.cs
@@ -16,8 +16,8 @@ public class Operation : MonoBehaviour{
         ReadNDEF
     }
     public class ChainedCommand {
-        List<byte> command;
-        List<byte> expectedReply;
+        List<byte> command = new List<byte>();
+        List<byte> expectedReply = new List<byte>();
     }
 
     public StandardComplexOperation standardComplexOperation;
