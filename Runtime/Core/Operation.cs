28d850116c27f5ea3d3e8ab47c4961cd22bc1d65
unknown <azfougues@gmail.com>
2024-07-21 18:19:44 -0400

    Lib update, implement operation processing

--- Runtime/Core/Operation.cs
+++ Runtime/Core/Operation.cs
@@ -2,26 +2,18 @@ using AbyssWalkerDev.NativeNFC;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-
+using static AbyssWalkerDev.NativeNFC.NFCTag;
 
 [Serializable]
 public class Operation : MonoBehaviour{
 
-    public enum StandardComplexOperation {
-        None,
-        ChainedCommand,
-        DeepScan,
-        PowerScan,
-        WriteNDEF,
-        ReadNDEF
-    }
     public class ChainedCommand {
         public List<byte> command = new List<byte>();
         public List<byte> expectedReply = new List<byte>();
     }
 
-    public StandardComplexOperation standardComplexOperation;
-    public NDEFContent ndefMessage;
+
+    public NFC_Technology technologyUsed;
     public List<ChainedCommand> commands;
 
 }
