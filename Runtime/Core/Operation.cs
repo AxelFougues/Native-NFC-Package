59205091362e45f7c9476ca4cea5ed291d23284d
unknown <azfougues@gmail.com>
2024-08-08 14:04:40 -0400

    Update lib

--- Runtime/Core/Operation.cs
+++ Runtime/Core/Operation.cs
@@ -6,13 +6,14 @@ using static AbyssWalkerDev.NativeNFC.NFCTag;
 
 [Serializable]
 public class Operation : MonoBehaviour{
+    public enum StandardOperation { COMMAND, NDEF_FORMAT }
 
     public class ChainedCommand {
         public List<byte> command = new List<byte>();
         public List<byte> expectedReply = new List<byte>();
     }
 
-
+    public StandardOperation standardOperation = StandardOperation.COMMAND;
     public NFC_Technology technologyUsed;
     public List<ChainedCommand> commands;
 
