82fccd5b0d2dc82e175495ce0d6d83c697404af7
unknown <azfougues@gmail.com>
2024-08-09 15:25:45 -0400

    Implement operations for all techs

--- Runtime/Core/Operation.cs
+++ Runtime/Core/Operation.cs
@@ -6,15 +6,16 @@ using static AbyssWalkerDev.NativeNFC.NFCTag;
 
 [Serializable]
 public class Operation : MonoBehaviour{
-    public enum StandardOperation { COMMAND, NDEF_FORMAT }
+
+    
+    public NFC_Technology technologyUsed = NFC_Technology.UNKNOWN;
+    public List<ChainedCommand> commands = new List<ChainedCommand>();
+
 
     public class ChainedCommand {
-        public List<byte> command = new List<byte>();
-        public List<byte> expectedReply = new List<byte>();
+        public byte[] command = null;
+        public byte[] expectedReply = null;
     }
 
-    public StandardOperation standardOperation = StandardOperation.COMMAND;
-    public NFC_Technology technologyUsed;
-    public List<ChainedCommand> commands;
 
 }
